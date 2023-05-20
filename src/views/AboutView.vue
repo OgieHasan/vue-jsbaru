cc6417e28f14d623c481dfba55d53c909d741bbe
OgieHasan <marionurrohman@student.uir.ac.id>
2023-05-20 20:33:13 +0700

    Hasan Files

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Biodata</h1>
+  <table>
+    <tr>
+      <th>Nama</th>
+      <td>Ogie Sepri Miftakhul Hasan</td>
+    </tr>
+    <tr>
+      <th>Tanggal Lahir</th>
+      <td>23 Oktober 2002</td>
+    </tr>
+    <tr>
+      <th>Alamat</th>
+      <td>Indragiri Hulu</td>
+    </tr>
+    <tr>
+      <th>Email</th>
+      <td>ogieseprimiftakhulhasan@student.uir.ac.id</td>
+    </tr>
+    <tr>
+      <th>No. Telepon</th>
+      <td>082285329945</td>
+    </tr>
+  </table>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
